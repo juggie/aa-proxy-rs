09a60f807e15ded6170a578d7e188a9f9f908724
George G <kreach3r@gmail.com>
2024-12-21 01:13:48 +0200

    usb_gadget.rs: increase UDC name to info logging
    This is too important information to be left out with the debug flag. Also, it doesn't produce a lot of output, so it won't clog the regular log.

--- src/usb_gadget.rs
+++ src/usb_gadget.rs
@@ -67,7 +67,7 @@ impl UsbGadgetState {
         if let Ok(entries) = fs::read_dir(&udc_dir) {
             for entry in entries {
                 if let Ok(entry) = entry {
-                    debug!("Using UDC: {:?}", entry.file_name());
+                    info!("Using UDC: {:?}", entry.file_name());
                     if let Ok(fname) = entry.file_name().into_string() {
                         state.udc_name.push_str(fname.as_str());
                         break;
